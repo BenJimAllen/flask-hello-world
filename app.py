bebb9ff5d204cbf03a9100269c2c4e108a62f5b9
BenJimAllen <benjamin.j.allen@colorado.edu>
2023-04-05 03:28:50 +0000

    Connected DB

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://benjimallen_lab_10_db_user:TVjoaGbULbV2sqsq4NUqHrxFGuGB0q2P@dpg-cgmejdrk9u59cru9dk70-a/benjimallen_lab_10_db")
+    conn.close()
+    return 'Database Connection Successful'
